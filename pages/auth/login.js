5de17f95313a9a35bca61cc08d6c01188f573d9d
rachel-wong <hello.rachelwong@gmail.com>
2022-10-26 18:03:51 +1000

    fix: fixed login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { auth } from '../../utils/firebase'
 
 const login = () => {
+
+  // Sign in with Google Auth
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider)
+    } catch (error) { }
+  }
+
   return (
     <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
       <h2 className="text-2xl font-medium">Join today</h2>
       <div className="py-4">
         <h3 className="py-4">Sign in with one of the providers</h3>
-        <button className="text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2">
+        <button onClick={GoogleLogin }  className="text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2">
           <FcGoogle className="text-2xl"/>
           <span>Sign in with Google</span></button>
       </div>
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase';
-import { getFireStore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,10 +12,10 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-export const db = getFireStore(app);
+export const db = getFirestore(app);
